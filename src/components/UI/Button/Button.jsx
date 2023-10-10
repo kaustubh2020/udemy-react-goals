0bf83a9092be8979a2dab7d22b915f3301414e9a
kaustubh2020 <kaustubhjaiswal200@gamil.com>
2023-10-10 22:23:02 +0530

    CSS modules

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -1,19 +1,21 @@
-/* import "./Button.css";
+//import "./Button.css";
+
+import styles from "./Button.module.css";
 
 const Button = (props) => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
 };
 
 export default Button;
- */
 
-import styled from "styled-components";
+/* import styled from "styled-components";
 
 const Button = styled.button`
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -21,6 +23,10 @@ const Button = styled.button`
   background: #8b005d;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
+
+  @media (min-width: 768) {
+    width: auto;
+  }
 
   &:focus {
     outline: none;
@@ -34,4 +40,4 @@ const Button = styled.button`
   }
 `;
 
-export default Button;
+export default Button; */
